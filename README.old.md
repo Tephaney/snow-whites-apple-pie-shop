006450c41124395824ab119061944fd6dc1728b7
Tephaney <101478420+Tephaney@users.noreply.github.com>
2022-06-25 21:10:41 -0700

    Navbar, SPA and React Router

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# snow-whites-apple-pie-shop
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home'>
+        <div className='headerContainer'>The Sweetest of Them All</div>
+    </div>
+  )
+}
+
+export default Home;
